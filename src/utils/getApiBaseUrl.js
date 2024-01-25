09f6974b75d1ec473942733218b649a69be6cde5
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-01-26 01:08:34 +0530

    second

--- src/utils/getApiBaseUrl.js
+++ src/utils/getApiBaseUrl.js
@@ -1,6 +1,7 @@
 const getApiBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:8000';
+    // return 'http://localhost:8000';
+    return 'https://todowithmern-project.onrender.com'
   }
   return import.meta.env.VITE_API_BASE_URL;
 };

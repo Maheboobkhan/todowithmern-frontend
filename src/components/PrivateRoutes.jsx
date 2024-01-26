7e0a3c74e0d093312b23976a21d5d39a80175151
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-01-26 18:36:52 +0530

    last

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -5,7 +5,7 @@ import useAuth from "../hooks/useAuth";
 function PrivateRoutes ({})  {
   const {auth} = useAuth();
 
-  if(auth === undefined) return "loading...";
+  // if(auth === undefined) return "loading...";
 
   return auth === true ? <Outlet /> : <Navigate to='/auth' />
 }
